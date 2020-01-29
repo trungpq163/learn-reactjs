b21493a94363b9a897224249f9d67e7eeb6018be
quoctrung163 <quoctrung163@yandex.com>
2020-01-29 14:38:25 +0700

    done 5-bt

---
+++ 5-bt/script.js
@@ -0,0 +1,8 @@
+// 1. Sử dụng create-react-app tạo 1 project như trong video
+// 2. Thử chạy: npm run build
+// 3. Thư mục mới nào được sinh ra sau khi chạy câu lệnh trên?
+// 4. Tác dụng của câu lệnh này là gì?
+// 5. Làm sao để deploy code đã build lên firebase hosting
+// Gợi ý: Không biết thì hỏi jsland hoặc slack 😎
+
+// tao folder buid
---
+++ todo-list/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+class TodoItem extends Component {
+    render() {
+        return (
+            <div className="App">
+                <p>Đi chợ</p>
+            </div>
+        );
+    }
+}
+export default TodoItem;
