0d658ce83e0c7a50b77a92c8b693147554940be2
Quốc Trung <miyucoder@gmail.com>
2020-02-01 18:58:43 +0700

    Update App.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
+// class App
 class App extends Component {
     constructor(){
         super();
