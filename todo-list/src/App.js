a19dffe48b3fa5b7ec16436e4d5217898055a653
quoctrung163 <quoctrung163@yandex.com>
2020-02-06 12:28:06 +0700

    create props.children

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,96 +5,102 @@ import tick from './img/survey.svg';
 
 // class App
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            newItems: '',
-            todoItems: [
-                { title: 'Mua bim bim', isComplete: true },
-                { title: 'Đi đá bóng', isComplete: true },
-                { title: 'Đi đổ xăng'}
-            ]
-        }
+	constructor() {
+		super();
+		this.state = {
+			newItems: '',
+			todoItems: [
+				{
+					title: 'Mua bim bim',
+					isComplete: true
+				},
+				{
+					title: 'Đi đá bóng',
+					isComplete: true
+				},
+				{
+					title: 'Đi đổ xăng'
+				}
+			]
+		};
 
-        this.onKeyUp = this.onKeyUp.bind(this);
-        this.onChange = this.onChange.bind(this);
-    }
-    onItemClicked(item) {
-        return (event) => {
-            const isComplete = item.isComplete;
-            const {todoItems} = this.state;
-            const index = todoItems.indexOf(item);
-            this.setState({
-                todoItems: [
-                    ...todoItems.slice(0, index),
-                    {
-                        ...item,
-                        isComplete: !isComplete
-                    },
-                    ...todoItems.slice(index + 1)
-                ]
-            });
-        }
-    }
+		this.onKeyUp = this.onKeyUp.bind(this);
+		this.onChange = this.onChange.bind(this);
+	}
+	onItemClicked(item) {
+		return (event) => {
+			const isComplete = item.isComplete;
+			const { todoItems } = this.state;
+			const index = todoItems.indexOf(item);
+			this.setState({
+				todoItems: [
+					...todoItems.slice(0, index),
+					{
+						...item,
+						isComplete: !isComplete
+					},
+					...todoItems.slice(index + 1)
+				]
+			});
+		};
+	}
 
-    onKeyUp = function(event) {
-        let text = event.target.value;
-        if (event.keyCode === 13) { // enter keyCode     
-            if (!text) {
-                return;
-            }
-            text = text.trim();
+	onKeyUp = function(event) {
+		let text = event.target.value;
+		if (event.keyCode === 13) {
+			// enter keyCode
+			if (!text) {
+				return;
+			}
+			text = text.trim();
 
-            if (!text) {
-                return;
-            }
+			if (!text) {
+				return;
+			}
 
-            this.setState({
-                todoItems: [
-                    { title: text, isComplete: false },
-                    ...this.state.todoItems,
+			this.setState({
+				todoItems: [
+					{
+						title: text,
+						isComplete: false
+					},
+					...this.state.todoItems
+				]
+			});
+		} else {
+			this.setState({
+				newItems: text
+			});
+		}
+	};
 
-                ]
-            })
-        } else {
-            this.setState({
-                newItems: text
-            })
-        }
-        
-    }
+	onChange(event) {
+		this.setState({
+			newItems: event.target.value
+		});
+	}
 
-    onChange(event) {
-        this.setState({
-            newItems: event.target.value
-        })
-    }
-
-    render() {
-        return (
-            <div className="App">
-               <div className="Header">
-                    <img src={tick} witdh={30} height={30}/>
-                    <input 
-                        type="text" 
-                        placeholder="Add a new item" 
-                        onKeyUp={this.onKeyUp} 
-                        value={this.state.newItems}    
-                        onChange={this.onChange}
-                    />
-               </div>
-                {
-                    this.state.todoItems.length > 0 && this.state.todoItems.map((item, index) => 
-                    <TodoItem key={index} 
-                    item={item}
-                    onClick={this.onItemClicked(item)}/>)
-                }
-                {
-                    this.state.todoItems.length === 0 && "Nothing here.."
-                }
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<div className="Header">
+					<img src={tick} witdh={30} height={30} />{' '}
+					<input
+						type="text"
+						placeholder="Add a new item"
+						onKeyUp={this.onKeyUp}
+						value={this.state.newItems}
+						onChange={this.onChange}
+					/>{' '}
+				</div>{' '}
+				{this.state.todoItems.length > 0 &&
+					this.state.todoItems.map((item, index) => (
+						<TodoItem key={index} item={item} onClick={this.onItemClicked(item)} />
+					))}{' '}
+				{this.state.todoItems.length === 0 && 'Nothing here..'}{' '}
+			</div>
+		);
+	}
 }
 
 export default App;
